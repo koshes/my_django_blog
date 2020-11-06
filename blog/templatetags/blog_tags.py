d1562a4dfd11407a8c1bf5fcc56f19f7a4924eb0
Yako <ysvolodin@gmail.com>
2020-11-06 19:17:19 +0300

    Добавлен пользовательский фильтр шаблонизатора для использования синтаксиса markdown в постах блога

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,7 +1,10 @@
 from django import template
 from django.db.models import Count
+from django.utils.safestring import mark_safe
 
 from ..models import Post
+
+import markdown
 
 register = template.Library()
 
@@ -21,3 +24,8 @@ def show_latest_posts(count=3):
 def get_most_commented_posts(count=3):
     return Post.published.annotate(
         total_comments=Count('comments')).order_by('-total_comments')[:count]
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
